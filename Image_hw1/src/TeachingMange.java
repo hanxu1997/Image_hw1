dc6a96cfd20884576996196862a6c7e6a3878e50
hanxu1997 <807174205@qqqqq.com>
2017-09-16 11:09:00 +0800

    init

---
+++ Image_hw1/src/TeachingMange.java
@@ -0,0 +1,73 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+public class TeachingMange{
+ 
+	JFrame frame = new JFrame();
+	Container content = frame.getContentPane();
+	JMenuBar menubar = new JMenuBar();
+	JMenu LoginMenu = new JMenu("系统登录");
+	JMenu UserMangeMenu = new JMenu("用户管理");
+	JMenu SchoolMangeMenu = new JMenu("学籍管理");
+	JMenu HelpMenu = new JMenu("关于");
+ 
+ 
+	public TeachingMange(){
+		JMenuItem userLoginMenu= new JMenuItem("用户登录");
+		userLoginMenu.addActionListener(new LoginActionListener());
+		JMenuItem exitLoginMenu= new JMenuItem("退出");
+		LoginMenu.add(userLoginMenu);
+		LoginMenu.add(exitLoginMenu);
+		menubar.add(LoginMenu);
+		menubar.add(UserMangeMenu);
+		menubar.add(SchoolMangeMenu);
+		menubar.add(HelpMenu);
+		frame.setTitle("imageprocessing");
+		content.add(menubar,BorderLayout.NORTH);
+		frame.setBounds(450, 200, 800, 800);
+		// frame.setBounds(450, 200, 400, 400);
+		frame.setVisible(true);
+	}
+ 
+	public class LoginActionListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+		   // TODO Auto-generated method stub
+		   JFrame frame01 = new JFrame();
+		   frame01.setTitle("用户登录");
+		   frame01.setBounds(450, 200, 400, 150);
+		   Container content01 = frame01.getContentPane();
+		   JPanel panel = new JPanel();
+		   JPanel panel01 = new JPanel();
+		   JPanel panel02 = new JPanel();
+		   JLabel label01 = new JLabel("请输入用户名: ");
+		   JLabel label02 = new JLabel("请输入密码:      ");
+		   JTextField text01 = new JTextField(20);
+		   JPasswordField text02 = new JPasswordField(20);
+		   panel01.add(label01);
+		   panel01.add(text01);
+		   panel02.add(label02);
+		   panel02.add(text02);
+		   panel.add(panel01);
+		   panel.add(panel02);
+		   content01.add(panel,BorderLayout.CENTER);
+		   //frame01.pack();
+		   frame01.setVisible(true);
+		}
+	}
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TeachingMange();
+	}
+}
