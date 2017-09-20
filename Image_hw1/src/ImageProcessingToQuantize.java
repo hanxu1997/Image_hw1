40dbb9ec19acac7250f0087b6021b9ff456765c0
hanxu1997 <807174205@qqqqq.com>
2017-09-20 23:31:54 +0800

    修改report

--- Image_hw1/src/ImageProcessingToQuantize.java
+++ Image_hw1/src/ImageProcessingToQuantize.java
@@ -10,35 +10,6 @@ public class ImageProcessingToQuantize {
 	public static final double G_FACTOR = 0.59;
 	public static final double B_FACTOR = 0.11;
 	
-	
-	/**
-	 * 利用灰度转换公式将原图转换为灰度图像
-	 * 注：所用输入图像类型为TYPE_BYTE_GRAY，已经是灰度图像，可以不用转化
-	 * @param originalImg
-	 * @return
-	 */
-	public static BufferedImage changeToGrayImg(BufferedImage originalImg) {
-		int width = originalImg.getWidth();
-        int height = originalImg.getHeight();
-        BufferedImage grayImg = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                int ARGB = originalImg.getRGB(j, i);
-                int A = (ARGB >> 24) & 0xFF;
-                int R = (ARGB >> 16) & 0xFF;
-                int G = (ARGB >> 8) & 0xFF;
-                int B = ARGB & 0xFF;
-                int gray = (int) (R * R_FACTOR + G * G_FACTOR + B * B_FACTOR);
-                int ARGB1 = ((A << 24) & 0xFF000000)
-                    | ((gray << 16) & 0x00FF0000)
-                    | ((gray << 8) & 0x0000FF00)
-                    | (gray & 0x000000FF);
-                grayImg.setRGB(j, i, ARGB1);
-            }
-        }
-        return grayImg;
-	}
-	
 	/**
 	 * 量化灰度图像
 	 * 遍历每个像素点，获得量化后像素值并set
@@ -91,7 +62,33 @@ public class ImageProcessingToQuantize {
 		return quantizedGray;
 	}
 	
-
+	/**
+	 * 利用灰度转换公式将原图转换为灰度图像
+	 * 注：所用输入图像类型为TYPE_BYTE_GRAY，已经是灰度图像，可以不用转化
+	 * @param originalImg
+	 * @return
+	 */
+	public static BufferedImage changeToGrayImg(BufferedImage originalImg) {
+		int width = originalImg.getWidth();
+        int height = originalImg.getHeight();
+        BufferedImage grayImg = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int ARGB = originalImg.getRGB(j, i);
+                int A = (ARGB >> 24) & 0xFF;
+                int R = (ARGB >> 16) & 0xFF;
+                int G = (ARGB >> 8) & 0xFF;
+                int B = ARGB & 0xFF;
+                int gray = (int) (R * R_FACTOR + G * G_FACTOR + B * B_FACTOR);
+                int ARGB1 = ((A << 24) & 0xFF000000)
+                    | ((gray << 16) & 0x00FF0000)
+                    | ((gray << 8) & 0x0000FF00)
+                    | (gray & 0x000000FF);
+                grayImg.setRGB(j, i, ARGB1);
+            }
+        }
+        return grayImg;
+	}
 	
 
 	
